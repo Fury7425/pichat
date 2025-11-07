721691a06ea0e197b18969f0821bbbd971ed4479
Fury7425 <fury78228@gmail.com>
2025-11-07 15:19:25 +0900

    Add cryptoClient service with crypto functions

---
+++ apps/mobile/src/services/cryptoClient.ts
@@ -0,0 +1,19 @@
+import {
+  createIdentity,
+  createPreKeys,
+  ensureSession,
+  encryptFor,
+  decryptFrom,
+  exportRecoveryKit,
+  importRecoveryKit
+} from '@pichat/crypto';
+
+export const cryptoClient = {
+  createIdentity,
+  createPreKeys,
+  ensureSession,
+  encryptFor,
+  decryptFrom,
+  exportRecoveryKit,
+  importRecoveryKit
+};
