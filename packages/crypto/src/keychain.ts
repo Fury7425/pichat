53e81f3b070a84e0108075af40ba48ec552d46c4
Fury7425 <fury78228@gmail.com>
2025-11-07 15:29:18 +0900

    Create keychain.ts

---
+++ packages/crypto/src/keychain.ts
@@ -0,0 +1,44 @@
+/* eslint-disable global-require */
+import { toBase64, fromBase64 } from '@pichat/utils';
+
+let Keychain: typeof import('react-native-keychain') | undefined;
+try {
+  // React Native runtime
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  Keychain = require('react-native-keychain');
+} catch (err) {
+  Keychain = undefined;
+}
+
+const memoryVault = new Map<string, string>();
+
+export async function storeSecret(ref: string, payload: Uint8Array): Promise<void> {
+  if (Keychain?.setInternetCredentials) {
+    await Keychain.setInternetCredentials(ref, ref, toBase64(payload), {
+      accessible: Keychain.ACCESSIBLE.WHEN_UNLOCKED_THIS_DEVICE_ONLY,
+      storage: Keychain.STORAGE_TYPE.AES
+    });
+    return;
+  }
+  memoryVault.set(ref, toBase64(payload));
+}
+
+export async function loadSecret(ref: string): Promise<Uint8Array | undefined> {
+  if (Keychain?.getInternetCredentials) {
+    const creds = await Keychain.getInternetCredentials(ref);
+    if (creds && creds.password) {
+      return fromBase64(creds.password);
+    }
+    return undefined;
+  }
+  const data = memoryVault.get(ref);
+  return data ? fromBase64(data) : undefined;
+}
+
+export async function deleteSecret(ref: string): Promise<void> {
+  if (Keychain?.resetInternetCredentials) {
+    await Keychain.resetInternetCredentials(ref);
+    return;
+  }
+  memoryVault.delete(ref);
+}
